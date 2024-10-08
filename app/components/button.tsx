8bc64927a883702c78f6b6088fff5e78ec2c5a87
bobi.rival <bahmadrival@gmail.com>
2024-10-08 15:19:06 +0700

    Add background image in login page

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,10 +1,11 @@
 interface ButtonProps {
   text: string;
+  className?: string;
 }
 
 export default function Button(props: ButtonProps) {
   return (
-    <button type="submit" className="flex-none rounded-md bg-secondary-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary-600">
+    <button type="submit" className={`${props.className} flex-none rounded-md bg-secondary-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary-600`}>
       {props.text}
     </button>
   );
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,25 +10,19 @@ export default function Login() {
   };
 
   return (
-    <section className="bg-no-repeat bg-cover bg-center bg-gray-700 bg-blend-multiply bg-opacity-60 min-h-screen">
+    <section className="bg-[url('https://ppiwarwick.org/image/Kabi.png')] bg-gray-700 bg-no-repeat bg-cover bg-center bg-blend-multiply bg-opacity-60 min-h-screen">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen pt:mt-0">
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-          <div className="space-y-4 md:space-y-6 p-8 md:p6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl">Sign in to your account</h1>
+          <div className="p-6 md:p-8">
+            <div className="flex justify-center gap-3">
+              <img src="https://ppiwarwick.org/logo.png" className="h-9" alt="PPIW Logo" />
+              <span className="self-center text-secondary-700 text-xl font-semibold whitespace-nowrap">PPI Warwick</span>
+            </div>
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl mb-6">Sign in to your account</h1>
             <form onSubmit={submitLogin} className="flex flex-col gap-4">
               <InputLabel name="username" type="text" placeHolder="" isRequired={true} />
               <InputLabel name="password" type="password" placeHolder="••••••••" isRequired={true} />
-              <Button text="Sign In" />
-              {/* <button type="submit" className="w-full mt-2 text-white bg-secondary-600 hover:bg-secondary-700 focus:ring-4 focus:outline-none focus:ring-secondary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                <div>
-                  <svg className="w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                  </svg>
-                  Sedang memproses login...
-                </div>
-                <div>Sign In</div>
-              </button> */}
+              <Button text="Sign In" className="mt-2" />
             </form>
           </div>
         </div>
