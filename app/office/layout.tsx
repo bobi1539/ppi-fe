885f71252ef6696356e7ab1e5dd5581605e5d5a1
bobi.rival <bahmadrival@gmail.com>
2024-10-15 19:43:57 +0700

    Add gray background when sidebar open

--- app/office/layout.tsx
+++ app/office/layout.tsx
@@ -16,6 +16,7 @@ export default function OfficeLayout({ children }: Readonly<{ children: React.Re
       <Topbar setIsSidebarOpen={handleSidebarOpen} />
       <Sidebar isSidebarOpen={isSidebarOpen} />
       <main className="md:ml-64 min-h-screen p-4 pt-20">{children}</main>
+      <div onClick={handleSidebarOpen} className={`${isSidebarOpen ? "bg-gray-900/50 fixed inset-0 z-30" : ""}`} />
     </div>
   );
 }
