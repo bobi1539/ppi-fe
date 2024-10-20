0e76104b3c471b3b299ddc26b38cfa4761e1f2fb
bobi.rival <bahmadrival@gmail.com>
2024-10-20 16:59:17 +0700

    Add key to img

--- app/office/event/page.tsx
+++ app/office/event/page.tsx
@@ -55,7 +55,7 @@ export default function Event() {
           {eventPages?.content.map((event) => (
             <div key={event.id} className="grid grid-cols-5 gap-2 justify-center md:flex md:flex-col cursor-pointer">
               <div className="col-span-2 flex justify-center">
-                <img className="w-auto md:w-full h-40 md:h-64 xl:h-96 rounded-lg " src={imageDownload(DIRECTORY_EVENT, event.cover)} alt={event.title} />
+                <img key={event.id} className="w-auto md:w-full h-40 md:h-64 xl:h-96 rounded-lg " src={imageDownload(DIRECTORY_EVENT, event.cover)} alt={`${event.title} ... ${event.id}`} />
               </div>
               <div className="col-span-3">
                 <h3 className="text-xl font-bold text-gray-900">{event.title}</h3>
