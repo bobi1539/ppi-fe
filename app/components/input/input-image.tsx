6e2914561975f08ab34cefd24502a1cc07d8179c
bobi.rival <bahmadrival@gmail.com>
2024-10-21 21:47:54 +0700

    Change img to next image

--- app/components/input/input-image.tsx
+++ app/components/input/input-image.tsx
@@ -2,6 +2,7 @@
 
 import { imageDownload } from "@/app/backend-api/file";
 import { DEFAULT_IMAGE_URL } from "@/app/constants/constant";
+import Image from "next/image";
 import { ChangeEvent, useEffect, useState } from "react";
 
 interface InputImageProps {
@@ -35,7 +36,7 @@ export default function InputImage(props: Readonly<InputImageProps>) {
 
   return (
     <div className="flex flex-col gap-4 justify-center items-center p-4">
-      <img className={props.classNameImagePreview} src={imagePreview && imagePreview !== null ? imagePreview : DEFAULT_IMAGE_URL} alt="profile" />
+      <Image className={props.classNameImagePreview} src={imagePreview && imagePreview !== null ? imagePreview : DEFAULT_IMAGE_URL} alt="profile" width={500} height={500} priority />
       <label htmlFor="upload-photo" className="flex justify-center items-center gap-2 px-2.5 py-1.5 text-sm text-white bg-gray-500 hover:bg-gray-400 rounded-lg transition duration-200 cursor-pointer">
         <i className="fa-solid fa-upload" />
         <span>{props.label ?? "Upload Photo"}</span>
--- app/office/components/topbar.tsx
+++ app/office/components/topbar.tsx
@@ -11,6 +11,7 @@ import { userFindByHeader } from "@/app/backend-api/user";
 import { UserResponse } from "@/app/dto/response/user-response";
 import { imageDownload } from "@/app/backend-api/file";
 import { DEFAULT_IMAGE_URL, DIRECTORY_USER } from "@/app/constants/constant";
+import Image from "next/image";
 
 interface TopbarProps {
   setIsSidebarOpen: () => void;
@@ -79,7 +80,7 @@ export default function Topbar(props: Readonly<TopbarProps>) {
           <DropdownMenu>
             <DropdownMenuTrigger type="button" className="outline-none hover:bg-gray-100 px-2 py-1 rounded-lg">
               <span className="sr-only">Open user menu</span>
-              <img className="w-8 h-8 rounded-full border border-gray-200" src={user?.photo && user.photo !== null ? photoUrl : DEFAULT_IMAGE_URL} alt="profile" />
+              <Image className="w-8 h-8 rounded-full border border-gray-200" src={user?.photo && user.photo !== null ? photoUrl : DEFAULT_IMAGE_URL} alt="profile" width={100} height={100} priority />
             </DropdownMenuTrigger>
             <DropdownMenuPortal>
               <DropdownMenuContent className="mt-5 w-56 text-base list-none bg-white divide-y divide-gray-100 shadow rounded-lg">
