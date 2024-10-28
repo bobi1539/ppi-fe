3c4d64c4af9bc228e4754becdde4d60b986ba7f4
bobi.rival <bahmadrival@gmail.com>
2024-10-28 09:19:28 +0700

    Fixing department action page

--- app/office/committee/department/page.tsx
+++ app/office/committee/department/page.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { PageResponse } from "@/app/dto/response/page-response";
 import { DivisionResponse } from "@/app/dto/response/division-response";
 import { DivisionSearchDto } from "@/app/dto/search/division-search-dto";
-import { CONSTANT_PAGE_SIZE_VALUE, ICON_DELETE, ICON_EDIT, ICON_RESTORE, TEXT_COLOR_DELETE, TEXT_COLOR_EDIT, TEXT_COLOR_RESTORE, TEXT_DELETE, TEXT_EDIT, TEXT_RESTORE } from "@/app/constants/constant";
+import { CONSTANT_PAGE_SIZE_VALUE, ICON_DELETE, ICON_EDIT, ICON_RESTORE, ICON_STAFF, TEXT_COLOR_DELETE, TEXT_COLOR_EDIT, TEXT_COLOR_RESTORE, TEXT_COLOR_STAFF, TEXT_DELETE, TEXT_EDIT, TEXT_RESTORE, TEXT_STAFF } from "@/app/constants/constant";
 import { divisionDelete, divisionFindAllPagination, divisionRestore } from "@/app/backend-api/division";
 import CustomTable from "@/app/components/table/custom-table";
 import { showConfirmDialog, showSuccessDialog } from "@/app/utils/sweet-alert";
@@ -131,8 +131,14 @@ export default function CommitteeDepartment() {
                 </td>
                 <td scope="row" className="px-2.5 py-2 whitespace-nowrap">
                   <CustomDropdown>
-                    <CustomDropdownItem onClick={() => handleViewStaff(division.id)} className={TEXT_COLOR_EDIT} icon="fa-solid fa-user-tie" text="Staff" />
-                    {division.deleted ? <CustomDropdownItem onClick={() => handleRestoreDivision(division.id)} className={TEXT_COLOR_RESTORE} icon={ICON_RESTORE} text={TEXT_RESTORE} /> : <CustomDropdownItem onClick={() => handleEditDivision(division.id)} className={TEXT_COLOR_EDIT} icon={ICON_EDIT} text={TEXT_EDIT} />}
+                    {division.deleted ? (
+                      <CustomDropdownItem onClick={() => handleRestoreDivision(division.id)} className={TEXT_COLOR_RESTORE} icon={ICON_RESTORE} text={TEXT_RESTORE} />
+                    ) : (
+                      <>
+                        <CustomDropdownItem onClick={() => handleViewStaff(division.id)} className={TEXT_COLOR_STAFF} icon={ICON_STAFF} text={TEXT_STAFF} />
+                        <CustomDropdownItem onClick={() => handleEditDivision(division.id)} className={TEXT_COLOR_EDIT} icon={ICON_EDIT} text={TEXT_EDIT} />
+                      </>
+                    )}
                     <CustomDropdownItem onClick={() => handleDeleteDivision(division.id)} className={TEXT_COLOR_DELETE} icon={ICON_DELETE} text={TEXT_DELETE} />
                   </CustomDropdown>
                 </td>
